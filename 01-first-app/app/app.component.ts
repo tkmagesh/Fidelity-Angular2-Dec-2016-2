ed9be98b0301dc48aeb47f5dad3b462cba8f6e8f
Magesh <tkmagesh77@gmail.com>
2016-12-28 10:33:54 +0530

    bug tracker - seed

--- 01-first-app/app/app.component.ts
+++ 01-first-app/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
+    <greet></greet>
     <salary-calculator></salary-calculator>
   `
 }) 
---
+++ 02-bug-tracker/app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `<h1>Bug Tracker</h1>`,
+})
+export class AppComponent  { name = 'Angular'; }
