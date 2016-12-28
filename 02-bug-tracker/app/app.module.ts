89e17675a979070d7382813ace44d0337ed7f10a
Magesh <tkmagesh77@gmail.com>
2016-12-28 16:12:15 +0530

    utils module

--- 02-bug-tracker/app/app.module.ts
+++ 02-bug-tracker/app/app.module.ts
@@ -1,18 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { UtilsModule }   from './utils/utils.module';
 
 import { AppComponent }  from './app.component';
 import { BugTrackerComponent } from './bug-tracker/bugTracker.component';
-import { TrimTextPipe } from './bug-tracker/pipes/trimText.pipe';
-import { SortPipe } from './bug-tracker/pipes/Sort.pipe';
 import { ClosedCountPipe } from './bug-tracker/pipes/closedCount.pipe';
 
 import { BugOperations } from './bug-tracker/services/BugOperations.service'
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, BugTrackerComponent, TrimTextPipe, SortPipe, ClosedCountPipe ],
+  imports:      [ BrowserModule, FormsModule, UtilsModule ],
+  declarations: [ AppComponent, BugTrackerComponent, ClosedCountPipe ],
   bootstrap:    [ AppComponent ],
   providers :   [ BugOperations ]
 })
