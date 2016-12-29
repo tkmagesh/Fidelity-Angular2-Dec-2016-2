2e956e48a7dea49838584275844b6a4daf1a9cd1
Magesh <tkmagesh77@gmail.com>
2016-12-29 14:25:54 +0530

    async patterns - part 1

---
+++ 03-async-patterns/async-patterns.js
@@ -0,0 +1,32 @@
+var pgm = (function(){
+    function addSync(x,y){
+        console.log(`   [Service] processing ${x} and ${y}`)
+        var result = x + y;
+        console.log(`   [Service] returning result`);
+        return result;
+    }
+    function addSyncClient(x,y){
+        console.log(`[Consumer] triggering addSync`);
+        var result = addSync(x,y);
+        console.log(`[Consumer] result = ${result}`);
+    }
+
+    function addAsync(x,y){
+        console.log(`   [Service] processing ${x} and ${y}`)
+        setTimeout(function(){
+            var result = x + y;
+            console.log(`   [Service] returning result`);
+            return result;
+        },5000);
+    }
+    function addAsyncClient(x,y){
+        console.log(`[Consumer] triggering addAsync`);
+        var result = addAsync(x,y);
+        console.log(`[Consumer] result = ${result}`);
+    }
+
+    return {
+        addSyncClient : addSyncClient,
+        addAsyncClient : addAsyncClient
+    }
+})();
